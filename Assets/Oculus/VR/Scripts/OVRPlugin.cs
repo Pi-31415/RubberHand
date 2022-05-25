9feff3f8d0e0c340566e01399a0373a2f49654e5
Pi <paingthetko.pi@gmail.com>
2022-05-25 15:30:53 +0400

    NullReferenceException Fixed
    Fixed NullReferenceException from Oculus package and Built Successfully

--- Assets/Oculus/VR/Scripts/OVRPlugin.cs
+++ Assets/Oculus/VR/Scripts/OVRPlugin.cs
@@ -41,6 +41,10 @@ public static partial class OVRPlugin
 	public const bool isSupportedPlatform = true;
 #endif
 
+private const string pluginName = "OVRPlugin";
+private static System.Version _versionZero = new System.Version(0, 0, 0);
+
+
 #if OVRPLUGIN_UNSUPPORTED_PLATFORM
 	public static readonly System.Version wrapperVersion = _versionZero;
 #else
@@ -6604,9 +6608,6 @@ public static partial class OVRPlugin
 #endif
 		}
 	}
-
-	private const string pluginName = "OVRPlugin";
-	private static System.Version _versionZero = new System.Version(0, 0, 0);
 
 	// Disable all the DllImports when the platform is not supported
 #if !OVRPLUGIN_UNSUPPORTED_PLATFORM
